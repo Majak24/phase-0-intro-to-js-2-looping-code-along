c8330b4f481705c1b1410c9875247f10065fd85a
Majak Deng <majakbanks.deng>
2024-07-25 16:46:09 +0300

    modified

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  
+    return gifts;
+  }
+  
+  function writeCards(names, event) {
+    const thankYouCards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouCards.push(message);
+    }
+  
+    return thankYouCards;
+  }
+  
+  function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  // Example usage
+  const gifts = ["teddy bear", "drone", "doll"];
+  wrapGifts(gifts);
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const thankYouCards = writeCards(names, "birthday");
+  console.log(thankYouCards);
